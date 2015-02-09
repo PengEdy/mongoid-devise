39143d9ead368e5c499dbefbc6a41b78872ad9d2
Alvin Qi <peng.edy@gmail.com>
2015-02-09 15:25:39 +0800

    add index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :email => 'foo@bar.com', :password => 'foobar', :password_confirmation => 'foobar'
+puts 'New user created: ' << user.email
+user2 = User.create! :email -> "bar@foo.com", :password => 'barfoo', :password_confirmation => 'barfoo'
+puts 'New user created: ' << user.email
