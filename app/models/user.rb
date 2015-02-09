d21b8bc408e92721b2da5fbfc26c2bf6e9a7b6b0
Alvin Qi <peng.edy@gmail.com>
2015-02-09 15:25:15 +0800

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  field :name
-  validates_presence_of :name
-  validates_uniqueness_of :name, :email, :case_sensitive => false
-  # attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
