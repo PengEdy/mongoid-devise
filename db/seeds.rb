14450c344d94750311ad866fcb7da76493b98947
Alvin Qi <peng.edy@gmail.com>
2015-02-09 15:26:25 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :email => 'foo@bar.com', :password => 'foobar', :password_confirmation => 'foobar'
 puts 'New user created: ' << user.email
-user2 = User.create! :email -> "bar@foo.com", :password => 'barfoo', :password_confirmation => 'barfoo'
+user2 = User.create! :email => "bar@foo.com", :password => 'barfoo', :password_confirmation => 'barfoo'
 puts 'New user created: ' << user.email
